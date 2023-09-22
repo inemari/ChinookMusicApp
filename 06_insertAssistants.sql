7642ca9c0e7eb6fc653cceea6762b48ee1130f64
inemari <140505495+inemari@users.noreply.github.com>
2023-09-22 17:38:55 +0200

    Delete 06_insertAssistants.sql

--- 06_insertAssistants.sql
+++
@@ -1,8 +0,0 @@
-Insert into Superhero(Name,Alias,Origin)
-Values('Tony Stark', 'Iron Man', 'New York')
-
-Insert into Superhero(Name,Alias,Origin)
-Values('Peter Parker', 'Spiderman', 'New York')
-
-Insert into Superhero(Name,Alias,Origin)
-Values('Bruce Wayne', 'Batman', 'Gotham City')
