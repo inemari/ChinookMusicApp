711d81f97873773ed3800a89148ccfce1aedbbea
inemari <140505495+inemari@users.noreply.github.com>
2023-09-22 17:39:36 +0200

    Delete 04_relationshipSuperheroPower.sql

--- 04_relationshipSuperheroPower.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE SuperheroPower (
-    SuperheroId INT,
-    PowerId INT,
-    PRIMARY KEY (SuperheroId, PowerId),
-    FOREIGN KEY (SuperheroId) REFERENCES Superhero(ID),
-    FOREIGN KEY (PowerId) REFERENCES Power(ID)
-);
