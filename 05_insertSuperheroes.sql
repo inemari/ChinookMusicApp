00b4799e3a45f84a8492cc3bd9841075c3000621
inemari <140505495+inemari@users.noreply.github.com>
2023-09-22 17:39:05 +0200

    Delete 05_insertSuperheroes.sql

--- 05_insertSuperheroes.sql
+++
@@ -1,8 +0,0 @@
-Insert into Assistant(Name, SuperID)
-Values('Jarvis', 1)
-
-Insert into Assistant(Name, SuperID)
-Values('Pepper Potts', 1)
-
-Insert into Assistant(Name, SuperID)
-Values('Robin', 3) 
